8665668592661c293d4ab9ab5e621fe83850b087
ljlVink <qpalzmlong@foxmail.com>
2023-11-05 17:46:51 +0800

    add comment

--- magisk_module/customize.sh
+++ magisk_module/customize.sh
@@ -1,5 +1,11 @@
 SKIPUNZIP=0
 
-ui_print "你妈的"
+ui_print "#####################"
+ui_print "TapFlow Project  -> WPS losetup_go install script "
+ui_print "By ljlVink"
+ui_print "本模块将在设备重启后生效。"
+ui_print "This module will take effect after the device is restarted."
+ui_print "Этот модуль вступит в силу после перезагрузки устройства."
+ui_print "#####################"
 
 extract "$ZIPFILE" 'losetup.sh' "$MODPATH"
